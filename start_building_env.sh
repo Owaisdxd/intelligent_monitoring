ecf8e9bb553d32e369afb3b0862a6e4fc0274158
owais_ahmed <owaisdxd@gmail.com>
2026-06-19 17:38:59 +0400

    Added conditional statement to verify if the env is exported

--- start_building_env.sh
+++ start_building_env.sh
@@ -3,71 +3,82 @@ echo "======================================="
 echo " LAUNCHING INEL OBSERVABILITY PLATFORM "
 echo "======================================="
 
-#1. Namespace Check & Creation
-echo "[INFO] Verifying Monitoring Namespace Layer..."
-kubectl create namespace monitoring --dry-run=client -o yaml | kubectl apply -f -
+read -p "Have you created var GITHUB_CLIENT_ID GITHUB_CLIENT_SECRET : (yes/no) " response
 
-#2. Automated SSL/TLS Engine Fix (The Gatekeeper)
-echo "[INFO] Securing Communication: Injecting TLS Credentials..."
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout /tmp/grafana.key \
-  -out /tmp/grafana.crt \
-  -subj "/CN=127.0.0.1/O=AIOps" \
-  -addext "subjectAltName=IP:127.0.0.1"
+if [[ "$response" == "yes" || "$response" == "y" || "$response" == "YES" ]]; then
+  echo "InelObservPlatform Evironment!!!"
 
-kubectl delete secret grafana-tls-secret -n monitoring --ignore-not-found
-kubectl create secret tls grafana-tls-secret \
-  --cert=/tmp/grafana.crt \
-  --key=/tmp/grafana.key \
-  -n monitoring
+  #1. Namespace Check & Creation
+  echo "[INFO] Verifying Monitoring Namespace Layer..."
+  kubectl create namespace monitoring --dry-run=client -o yaml | kubectl apply -f -
 
-echo "TLS Secret creation confirmation"
-kubectl get secret grafana-tls-secret -n monitoring
+  #2. Automated SSL/TLS Engine Fix (The Gatekeeper)
+  echo "[INFO] Securing Communication: Injecting TLS Credentials..."
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /tmp/grafana.key \
+    -out /tmp/grafana.crt \
+    -subj "/CN=127.0.0.1/O=AIOps" \
+    -addext "subjectAltName=IP:127.0.0.1"
 
-echo $YOUR_GITHUB_CLIENT_ID
-echo $YOUR_GITHUB_CLIENT_SECRET
+  kubectl delete secret grafana-tls-secret -n monitoring --ignore-not-found
+  kubectl create secret tls grafana-tls-secret \
+    --cert=/tmp/grafana.crt \
+    --key=/tmp/grafana.key \
+    -n monitoring
 
-kubectl create secret generic grafana-github-oauth \
-  --from-literal=client_id=$YOUR_GITHUB_CLIENT_ID \
-  --from-literal=client_secret=$YOUR_GITHUB_CLIENT_SECRET \
-  -n monitoring
-#Core Networking, Storage and Service Accounts
-kubectl apply -f k8s-manifests/service_account.yaml -n monitoring
-kubectl apply -f k8s-manifests/grafana_rbac.yaml
-kubectl apply -f k8s-manifests/fluent-bit_rbac.yaml
+  echo "TLS Secret creation confirmation"
+  kubectl get secret grafana-tls-secret -n monitoring
 
-#Configuration
-kubectl apply -f k8s-manifests/prometheus_cm.yaml
-kubectl apply -f k8s-manifests/prometheus-rules.yaml
-kubectl apply -f k8s-manifests/alertmanager_cm.yaml
-kubectl apply -f k8s-manifests/otel-collector_cm.yaml
-kubectl apply -f k8s-manifests/fluent-bit_cm.yaml
-kubectl apply -f k8s-manifests/jaeger_cm.yaml
-kubectl apply -f k8s-manifests/grafana-dashboards.yaml
+  echo $YOUR_GITHUB_CLIENT_ID
+  echo $YOUR_GITHUB_CLIENT_SECRET
 
-#Core Backend Services Infrastructure Triggers
-kubectl apply -f k8s-manifests/fluent-bit-ds.yaml
-kubectl apply -f k8s-manifests/prometheus_deploy.yaml
-kubectl apply -f k8s-manifests/prometheus_svc.yaml
-kubectl apply -f k8s-manifests/jaeger_deploy.yaml
-kubectl apply -f k8s-manifests/alertmanager_deploy.yaml
-kubectl apply -f k8s-manifests/alertmanager_svc.yaml
-kubectl apply -f k8s-manifests/otel-collector_deploy.yaml
-kubectl apply -f k8s-manifests/otel-collector_svc.yaml
-kubectl apply -f k8s-manifests/blackbox_deploy.yaml
-kubectl apply -f k8s-manifests/blackbox_svc.yaml
-kubectl apply -f k8s-manifests/loki_deploy.yaml
-kubectl apply -f k8s-manifests/loki_svc.yaml
-kubectl apply -f k8s-manifests/node-exporter_deploy.yaml
-kubectl apply -f k8s-manifests/node-exporter_svc.yaml
+  kubectl create secret generic grafana-github-oauth \
+    --from-literal=client_id=$YOUR_GITHUB_CLIENT_ID \
+    --from-literal=client_secret=$YOUR_GITHUB_CLIENT_SECRET \
+    -n monitoring
+  #Core Networking, Storage and Service Accounts
+  kubectl apply -f k8s-manifests/service_account.yaml -n monitoring
+  kubectl apply -f k8s-manifests/grafana_rbac.yaml
+  kubectl apply -f k8s-manifests/fluent-bit_rbac.yaml
 
-#Grafana Portal Deployment and Storage
-kubectl apply -f k8s-manifests/grafana_deploy.yaml
-kubectl apply -f k8s-manifests/grafana_svc.yaml
-kubectl apply -f k8s-manifests/grafana_ingress.yaml
-kubectl apply -f k8s-manifests/grafana-storage.yaml
-kubectl apply -f k8s-manifests/prometheus_storage.yaml
-kubectl apply -f k8s-manifests/jaeger_storage.yaml
+  #Configuration
+  kubectl apply -f k8s-manifests/prometheus_cm.yaml
+  kubectl apply -f k8s-manifests/prometheus-rules.yaml
+  kubectl apply -f k8s-manifests/alertmanager_cm.yaml
+  kubectl apply -f k8s-manifests/otel-collector_cm.yaml
+  kubectl apply -f k8s-manifests/fluent-bit_cm.yaml
+  kubectl apply -f k8s-manifests/jaeger_cm.yaml
+  kubectl apply -f k8s-manifests/grafana-dashboards.yaml
 
-echo "============================================="
-echo "InelObservPlatform Evironment is Setted Up Successfully"
+  #Core Backend Services Infrastructure Triggers
+  kubectl apply -f k8s-manifests/fluent-bit-ds.yaml
+  kubectl apply -f k8s-manifests/prometheus_deploy.yaml
+  kubectl apply -f k8s-manifests/prometheus_svc.yaml
+  kubectl apply -f k8s-manifests/jaeger_deploy.yaml
+  kubectl apply -f k8s-manifests/alertmanager_deploy.yaml
+  kubectl apply -f k8s-manifests/alertmanager_svc.yaml
+  kubectl apply -f k8s-manifests/otel-collector_deploy.yaml
+  kubectl apply -f k8s-manifests/otel-collector_svc.yaml
+  kubectl apply -f k8s-manifests/blackbox_deploy.yaml
+  kubectl apply -f k8s-manifests/blackbox_svc.yaml
+  kubectl apply -f k8s-manifests/loki_deploy.yaml
+  kubectl apply -f k8s-manifests/loki_svc.yaml
+  kubectl apply -f k8s-manifests/node-exporter_deploy.yaml
+  kubectl apply -f k8s-manifests/node-exporter_svc.yaml
+
+  #Grafana Portal Deployment and Storage
+  kubectl apply -f k8s-manifests/grafana_deploy.yaml
+  kubectl apply -f k8s-manifests/grafana_svc.yaml
+  kubectl apply -f k8s-manifests/grafana_ingress.yaml
+  kubectl apply -f k8s-manifests/grafana-storage.yaml
+  kubectl apply -f k8s-manifests/prometheus_storage.yaml
+  kubectl apply -f k8s-manifests/jaeger_storage.yaml
+
+  echo "============================================="
+  echo "InelObservPlatform Evironment is Setted Up Successfully"
+else
+    echo "Kindly first create var for grafana "
+    exit 1
+fi
+
+echo "Script completed successfully!"
